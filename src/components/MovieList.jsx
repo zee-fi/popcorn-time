00a8f7d4b474fb14f921749de01a4f09c3e40c5d
fiona-eh <fionahulse@mac.com>
2024-10-23 14:07:27 +0200

    implement delete button and movie counter

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import movies from "../data/movies.json";
 
 function MovieList() {
+
+    const [moviesToDisplay, setmoviesToDisplay] = useState(movies);
+
+    const deleteMovie = (movieId) => {
+        const newArr = moviesToDisplay.filter((movies) => {
+            return movies.id !== movieId;
+        });
+        setmoviesToDisplay(newArr);
+    }
+
     return (
         <>
             <div>
-                <h1>List of movies:</h1>
-                {movies.map((movieObj) => {
+                <h2>We currently have {moviesToDisplay.length} movies available</h2>
+                {moviesToDisplay.map((movieObj) => {
                     return <div key={movieObj.id} className="card">
                         <img src={movieObj.imgURL} />
                         <h3>{movieObj.title}</h3>
                         <p>Year: {movieObj.year}</p>
                         <p>Rating: {movieObj.rating}</p>
+
+                        <p>
+                            <button onClick={() => { deleteMovie(movieObj.id) }}>Delete this movie</button>
+                        </p>
                     </div>
                 })}
             </div>
